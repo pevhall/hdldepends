d4902feeafa86f601a375548cd4ba4288b5b6bc7
Pev Hall <pev.hall@gmail.com>
2025-02-03 17:24:29 +1030

    Ungly handing of top level library
    I've made the ability to set the top level library to get around issues
    where the use of the library was really unitnended. Like 99% of the
    files everything references 'work' but then one references
    'xil_defaultlib' instead.
    
    I'm gong to have a think about how better the handle this issue. Using
    the command line option in this file is a temprary work around

--- src/hdldepends/hdldepends.py
+++ src/hdldepends/hdldepends.py
@@ -113,10 +113,10 @@ def str_to_name(s: str):
 
 
 class Lookup:
-    def get_package(self, name: Name):
+    def get_package(self, name: Name, f_obj_required_by : Optional["FileObj"]):
         pass
 
-    def get_entity(self, name: Name):
+    def get_entity(self, name: Name, f_obj_required_by : Optional["FileObj"]):
         pass
 
     def add_loc(self, loc: Path, f_obj: "FileObj"):
@@ -155,12 +155,12 @@ class FileObj:
         file_deps = []
 
         for p in self.package_deps:
-            f_obj = look.get_package(p)
+            f_obj = look.get_package(p, self)
             if f_obj is not None:
                 self._add_to_f_deps(file_deps, f_obj)
 
         for e in self.entity_deps:
-            f_obj = look.get_entity(e)
+            f_obj = look.get_entity(e, self)
             if f_obj is not None:
                 self._add_to_f_deps(file_deps, f_obj)
 
@@ -173,6 +173,10 @@ class FileObj:
         self, look: Lookup, files_passed=[], level=0
     ) -> list["FileObj"]:
         files_passed.append(self)
+        # top_lib = look.get_top_lib()
+        # if top_lib is not None:
+        #     if top_lib == self.lib:
+        #         self.lib = top_lib
         order: list["FileObj"] = []
         for f_obj in self.get_file_deps(look):
             if f_obj not in files_passed:
@@ -259,6 +263,7 @@ def make_set(v) -> set:
 
 class LookupSingular(Lookup):
     TOML_KEYS = [
+        "top_lib",
         "ignore_libs",
         "ignore_packages",
         "ignore_entities",
@@ -269,6 +274,7 @@ class LookupSingular(Lookup):
     ]
 
     def __init__(self, allow_duplicates: bool = True):
+        log.debug('LookupSingular::__init__')
         self.allow_duplicates = allow_duplicates
         self.package_name_2_file_obj: dict[Name, FileObjLookup] = {}
         self.entity_name_2_file_obj: dict[Name, FileObjLookup] = {}
@@ -279,6 +285,7 @@ class LookupSingular(Lookup):
         self.toml_loc: Optional[Path] = None
         self.toml_modification_time = None
         self.dependency_files_md5sum : dict [Path : int ] = {}
+        self.top_lib : Optional[str] = None
 
     def _add_to_dict(self, d: dict, key, f_obj: FileObj):
         log.info(f'Adding {key} to dict')
@@ -395,45 +402,54 @@ class LookupSingular(Lookup):
         return result
 
     def extract_set_name_from_config(
-        config: dict, key: str
+        config: dict, key: str, top_lib = Optional[str]
     ) -> set[Name]:
         l = []
 
         def call_back_func(lib: str, name: str):
+            if top_lib is not None and lib == 'work':
+                lib = top_lib;
             l.append(Name(lib=lib, name=name))
 
         LookupSingular._process_config_opt_lib(config, key, call_back_func)
         log.info(f"{key} = {l}")
         return set(l)
 
-    def initalise_from_config_dict(self, config: dict, work_dir : Path):
+    def initalise_from_config_dict(self, config: dict, work_dir : Path, top_lib : Optional[str]):
+        if 'top_lib' in config:
+            self.top_lib = config['top_lib']
         self.ignore_set_libs = LookupSingular.extract_set_str_from_config(
             config, "ignore_libs"
         )
         self.ignore_set_packages = LookupSingular.extract_set_name_from_config(
-            config, "ignore_packages"
+            config, "ignore_packages", top_lib=top_lib
         )
         self.ignore_set_entities = LookupSingular.extract_set_name_from_config(
-            config, "ignore_entities"
+            config, "ignore_entities", top_lib=top_lib
         )
         file_list = self.get_file_list_from_config_dict(
-            config, work_dir
+            config, work_dir, top_lib
         )
         self.register_file_list(file_list)
         
-        self.update_external_deps_from_config_dict(config, work_dir)
+        self.update_external_deps_from_config_dict(config, work_dir, top_lib=top_lib)
 
-    def get_file_list_from_config_dict(self, config: dict, work_dir: Path):
+    def get_file_list_from_config_dict(self, config: dict, work_dir: Path, top_lib : Optional[str]):
 
+        log.debug(f'called get_file_list_from_config_dict( top_lib={top_lib} )')
         file_list: list[tuple(str, Path)] = []
 
         def add_file_to_list(lib, loc_str):
+            if top_lib is not None and lib == 'work':
+                lib = top_lib;
             loc = path_from_dir(work_dir, Path(loc_str))
             file_list.append((lib, loc))
 
         LookupSingular._process_config_opt_lib(config, "files", add_file_to_list)
 
         def add_file_list_to_list(lib, f_str):
+            if top_lib is not None and lib == 'work':
+                lib = top_lib;
             fl_loc = Path(f_str)
             fl_loc = path_from_dir(work_dir, fl_loc)
             with open(fl_loc, "r") as f_list_file:
@@ -447,10 +463,16 @@ class LookupSingular(Lookup):
             config, "file_list_files", add_file_list_to_list
         )
 
+        print('tetes')
+        for lib, f in file_list:
+            print(f'{lib, f}')
+
         return file_list
         
-    def update_external_deps_from_config_dict(self, config: dict, work_dir: Path):
+    def update_external_deps_from_config_dict(self, config: dict, work_dir: Path, top_lib : Optional[str]):
         def add_ext_dep_file_to_list(lib, f_str):
+            if top_lib is not None and lib == 'work':
+                lib = top_lib;
             fl_loc = Path(f_str)
             fl_loc = path_from_dir(work_dir, fl_loc)
             with open(fl_loc, "r") as f_list_file:
@@ -470,10 +492,10 @@ class LookupSingular(Lookup):
 
 
     @staticmethod
-    def create_from_config_dict(config: dict, work_dir: Path):
+    def create_from_config_dict(config: dict, work_dir: Path, top_lib : Optional[str]):
 
         inst = LookupSingular()
-        inst.initalise_from_config_dict(config, work_dir)
+        inst.initalise_from_config_dict(config, work_dir, top_lib=top_lib)
         return inst
 
     def register_file_list(self,file_list):
@@ -499,43 +521,55 @@ class LookupSingular(Lookup):
         loc = loc.resolve()
         return loc in self.loc_2_file_obj
 
-    def get_package(self, name: Name) -> Optional[FileObj]:
+    def get_package(self, name: Name, f_obj_required_by : Optional[FileObj]) -> Optional[FileObj]:
+        loc_str = 'None'
+        if f_obj_required_by is not None:
+            loc_str = str(f_obj_required_by.loc)
         if name not in self.package_name_2_file_obj:
             if name.lib in self.ignore_set_libs:
                 return None
             if name in self.ignore_set_packages:
                 return None
-            raise KeyError(f"ERROR: Could not find package {name}")
+            raise KeyError(f"ERROR: Could not find package {name} required by {loc_str}")
 
         item = self.package_name_2_file_obj[name]
         if isinstance(item, FileObj):
             return item
         elif isinstance(item, ConflictFileObj):
             item.log_confict(name)
-            raise KeyError(f"ERROR: confict on package {name}")
+            raise KeyError(f"ERROR: confict on package {name} required by {loc_str}")
 
-    def get_entity(self, name: Name) -> Optional[FileObj]:
+    def get_entity(self, name: Name, f_obj_required_by : Optional[FileObj]) -> Optional[FileObj]:
+        loc_str = 'None'
+        if f_obj_required_by is not None:
+            loc_str = str(f_obj_required_by.loc)
         if name not in self.entity_name_2_file_obj:
             if name.lib in self.ignore_set_libs:
                 return None
 
             if name in self.ignore_set_entities:
                 return None
-            raise KeyError(f"ERROR: Could not find entity {name}")
+            raise KeyError(f"ERROR: Could not find entity {name} required by {loc_str}")
 
         item = self.entity_name_2_file_obj[name]
         if isinstance(item, FileObj):
             return item
         elif isinstance(item, ConflictFileObj):
             item.log_confict(name)
-            raise KeyError(f"ERROR: confict on entity {name}")
+            raise KeyError(f"ERROR: confict on entity {name} required by {loc_str}")
 
+    def get_top_lib(self):
+        return self.top_lib
+
+    def set_top_lib(self, top_lib : Optional[str]):
+        self.top_lib = top_lib
 
 class LookupMulti(LookupSingular):
     TOML_KEYS = ["sub_toml"]
 
     def __init__(
             self, look_subs: list[LookupSingular]): #, file_list: list[tuple[str, Path]] = [] ):
+        log.debug('LookupMulti::__init__')
         self.look_subs = look_subs
         super().__init__(allow_duplicates=False)
         self.f_obj_top = None
@@ -543,15 +577,15 @@ class LookupMulti(LookupSingular):
 
     @staticmethod
     def create_from_config_dict(
-        config: dict, work_dir: Path, look_subs=[]
+            config: dict, work_dir: Path, look_subs=[], top_lib : Optional[str] = None
     ):
 
         look = LookupMulti(look_subs)
-        look.initalise_from_config_dict(config, work_dir)
+        look.initalise_from_config_dict(config, work_dir, top_lib=top_lib)
         return look
 
     def register_file_list(
-        self, file_list: list[tuple[str, Path]]
+            self, file_list: list[tuple[str, Path]]
     ):
         for lib, loc in file_list:
             f_obj = self._get_loc_from_common(loc)
@@ -584,31 +618,44 @@ class LookupMulti(LookupSingular):
                 return l_common.get_loc(loc)
         return None
 
+    def get_top_lib(self) -> Optional[str]:
+        if super().get_top_lib() is not None:
+            return super().get_top_lib()
+        for l_common in self.look_subs:
+            top_lib = l_common.get_top_lib()
+            if top_lib is not None:
+                return top_lib
+        return None
+
     def _get_named_item(
-        self, item_ref: str, name: Name, call_back_func_arr
+        self, item_ref: str, name: Name, call_back_func_arr, f_obj_required_by : Optional[FileObj]
     ) -> Optional[FileObj]:
         for call_back in call_back_func_arr:
             try:
-                result = call_back(name)
+                result = call_back(name, f_obj_required_by)
                 return result
             except KeyError:
                 pass
 
-        raise KeyError(f"{item_ref} {name} not found in depndency lookups")
 
-    def get_package(self, name: Name) -> Optional[FileObj]:
-        def cb(name: Name):
-            return LookupSingular.get_package(self, name)
+        loc_str = 'None'
+        if f_obj_required_by is not None:
+            loc_str = f'{f_obj_required_by.lib}:{f_obj_required_by.loc}'
+        raise KeyError(f"{item_ref} {name} not found in depndency lookups. required by file {loc_str}")
+
+    def get_package(self, name: Name, f_obj_required_by : Optional[FileObj]) -> Optional[FileObj]:
+        def cb(name: Name, f_obj_required_by : Optional[FileObj]):
+            return LookupSingular.get_package(self, name, f_obj_required_by)
 
         call_back_func_arr = [cb] + [l.get_package for l in self.look_subs]
-        return self._get_named_item("package", name, call_back_func_arr)
+        return self._get_named_item("package", name, call_back_func_arr, f_obj_required_by)
 
-    def get_entity(self, name: Name) -> Optional[FileObj]:
-        def cb(name: Name):
-            return LookupSingular.get_entity(self, name)
+    def get_entity(self, name: Name, f_obj_required_by : Optional[FileObj]) -> Optional[FileObj]:
+        def cb(name: Name, f_obj_required_by : Optional[FileObj]):
+            return LookupSingular.get_entity(self, name, f_obj_required_by)
 
         call_back_func_arr = [cb] + [l.get_entity for l in self.look_subs]
-        return self._get_named_item("entity", name, call_back_func_arr)
+        return self._get_named_item("entity", name, call_back_func_arr, f_obj_required_by)
 
 
 class LookupPrj(LookupMulti):
@@ -616,23 +663,31 @@ class LookupPrj(LookupMulti):
 
     def __init__(
             self, look_subs: list[LookupMulti]) : #, file_list: list[tuple[str, Path]] = [] ):
+        log.debug('LookupPrj::__init__')
         super().__init__(look_subs)
         self.f_obj_top = None
         self._compile_order = None
 
+    def set_top_lib(self, top_lib : Optional[str]):
+        self._compile_order = None
+        super().set_top_lib(top_lib)
+
     @staticmethod
     def create_from_config_dict(
-        config: dict, work_dir: Path, look_subs=[]
+            config: dict, work_dir: Path, look_subs=[], top_lib : Optional[str] = None
     ):
 
         look = LookupPrj(look_subs)
-        look.initalise_from_config_dict(config, work_dir)
+        look.initalise_from_config_dict(config, work_dir, top_lib=top_lib)
 
         if "top_file" in config:
 
             l = []
 
             def call_back_func(lib: str, loc_str: str):
+                #TODO!!!!
+                if top_lib is not None and lib == 'work':
+                    lib = top_lib;
                 loc_str = work_dir / loc_str
                 n = (lib, loc_str)
                 if len(l) != 0:
@@ -722,14 +777,20 @@ def parse_vhdl_file(look: Optional[Lookup], loc: Path, lib="work"):
                         log.info(f"\tcomponent_inst: {name}")  # Extract component name
                 case "direct_inst":
                     for item in found:
-                        name = Name(item[1], item[2])
+                        l = item[1]
+                        if l == 'work':
+                            l = lib
+                        name = Name(l, item[2])
                         f_obj.entity_deps.append(name)
                         log.info(
                                 f"\tdirect_inst {name}"
                             )  # Extract library and component names
                 case "package_use":
                     for item in found:
-                        name = Name(item[0], item[1])
+                        l = item[0]
+                        if l == 'work':
+                            l = lib
+                        name = Name(l, item[1])
                         f_obj.package_deps.append(name)
                         log.info(
                             f"\tpackage_use {name}"
@@ -757,9 +818,9 @@ def issue_key(good_keys: list, keys: list) -> Optional[str]:
 
 
 def create_lookup_from_toml(
-    toml_loc: Path, work_dir: Optional[Path] = None, attemp_read_pickle = True, write_pickle = True, force_LookupPrj=False
+    toml_loc: Path, work_dir: Optional[Path] = None, attemp_read_pickle = True, write_pickle = True, force_LookupPrj=False, top_lib : Optional[str]= None
 ):
-    log.debug(f'toml_loc {toml_loc} , work_dir {work_dir}, attemp_read_pickle {attemp_read_pickle}, write_pickle {write_pickle}, force_LookupPrj {force_LookupPrj}')
+    log.debug(f'toml_loc {toml_loc} , work_dir {work_dir}, attemp_read_pickle {attemp_read_pickle}, write_pickle {write_pickle}, force_LookupPrj {force_LookupPrj}, top_lib {top_lib}')
     if toml_loc.suffix != '.toml':
         if len(toml_loc.suffix) == 0:
             log.info('f adding .toml suffix/extension to {toml_loc}')
@@ -796,7 +857,7 @@ def create_lookup_from_toml(
         for loc in c_locs:
             loc = Path(loc)
             look_subs.append(
-                create_lookup_from_toml(loc, work_dir, attemp_read_pickle=attemp_read_pickle, write_pickle=write_pickle)
+                create_lookup_from_toml(loc, work_dir, attemp_read_pickle=attemp_read_pickle, write_pickle=write_pickle, top_lib=top_lib)
             )
 
         # log.info(f"create LookupPrj from {toml_loc}")
@@ -821,14 +882,14 @@ def create_lookup_from_toml(
 
         log.info(f"create LookupPrj from {toml_loc}")
         inst = LookupPrj.create_from_config_dict(
-            config, work_dir=work_dir, look_subs=look_subs
+            config, work_dir=work_dir, look_subs=look_subs, top_lib=top_lib
         )
 
     elif contains_any(config.keys(), LookupMulti.TOML_KEYS):
 
         log.info(f"create LookupMulti from {toml_loc}")
         inst = LookupMulti.create_from_config_dict(
-            config, work_dir=work_dir, look_subs=look_subs
+            config, work_dir=work_dir, look_subs=look_subs, top_lib=top_lib
         )
 
     else :
@@ -837,7 +898,7 @@ def create_lookup_from_toml(
         with open(toml_loc, "rb") as toml_f:
             config = tomllib.load(toml_f)
             inst = LookupSingular.create_from_config_dict(
-                config, work_dir=work_dir
+                config, work_dir=work_dir, top_lib=top_lib
             )
 
     print(f'toml_loc {toml_loc}')
@@ -883,6 +944,7 @@ if __name__ == "__main__":
         help="Paths to / File Names of, the config TOML input file(s).",
     )
     parser.add_argument("--top-file", type=str, help="top level file to use")
+    parser.add_argument("--top-lib", type=str, help="top level library")
     parser.add_argument(
         "--compile-order", type=str, help="Path to the compile order output file."
     )
@@ -902,26 +964,30 @@ if __name__ == "__main__":
     log.debug("no pickle", args.no_pickle)
     # print("File Dependencies:", args.file_dependencies)
     work_dir=Path('.')
+    top_lib = None
+    if args.top_lib:
+        top_lib = args.top_lib
 
     attemp_read_pickle = not args.clear_pickle and not args.no_pickle
     write_pickle = not args.no_pickle
     if len(args.config_toml) == 1:
         log.debug('creating top level project toml')
         look = create_lookup_from_toml(Path(args.config_toml[0]), work_dir=work_dir,
-           force_LookupPrj=True, attemp_read_pickle=attemp_read_pickle, write_pickle=write_pickle
+           force_LookupPrj=True, attemp_read_pickle=attemp_read_pickle, write_pickle=write_pickle, top_lib=top_lib
        )
     else:
         look_subs = []
         for c_toml in look_subs:
             look_subs.append(
                 create_lookup_from_toml(
-                    Path(c_toml), work_dir=work_dir, attemp_read_pickle=attemp_read_pickle, write_pickle=write_pickle
+                    Path(c_toml), work_dir=work_dir, attemp_read_pickle=attemp_read_pickle, write_pickle=write_pickle, top_lib=top_lib
                 )
             )
         look = LookupPrj(look_subs)
 
     if args.top_file:
         look.set_top_file(Path(args.top_file), "work")
+
 
     if look.has_top_file():
         look.print_compile_order()
